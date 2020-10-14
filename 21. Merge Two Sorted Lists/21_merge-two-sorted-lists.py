04cbf46bbc5fc32078165d68b274eca3b185839a
Wei ZHANG <yidazhang1@gmail.com>
2020-10-14 20:08:00 +0800

    Update and rename 1_two-sum.py to 21_merge-two-sorted-lists.py

---
+++ 21. Merge Two Sorted Lists/21_merge-two-sorted-lists.py
@@ -0,0 +1,35 @@
+%递归
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+        
+%迭代     
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        prehead = ListNode(-1)
+
+        prev = prehead
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next            
+            prev = prev.next
+
+        # 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        prev.next = l1 if l1 is not None else l2
+
+        return prehead.next
+
+
