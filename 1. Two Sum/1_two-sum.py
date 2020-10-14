6759ccfbec612cae5ed20ec61836d24fd210e408
Wei ZHANG <yidazhang1@gmail.com>
2020-10-14 20:27:45 +0800

    Update 1_two-sum.py

--- 1. Two Sum/1_two-sum.py
+++ 1. Two Sum/1_two-sum.py
@@ -1,22 +1,31 @@
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m = len(matrix)
-        if m == 0:
-            return False
-        n = len(matrix[0])
-        
-        #binary search
-        left, right = 0, m * n - 1
-        while left <= right:
-                pivot_idx = (left + right) // 2
-                pivot_element = matrix[pivot_idx // n][pivot_idx % n]
-                if target == pivot_element:
-                    return True
-                else:
-                    if target < pivot_element:
-                        right = pivot_idx - 1
-                    else:
-                        left = pivot_idx + 1
-        return False
+def twoSum(nums, target):
+```
+soluton1:
+```
+    lens = len(nums)
+    j=-1
+    for i in range(lens):
+        if (target - nums[i]) in nums:
+            if (nums.count(target - nums[i]) == 1)&(target - nums[i] == nums[i]):#如果num2=num1,且nums中只出现了一次，说明找到是num1本身。
+                continue
+            else:
+                j = nums.index(target - nums[i],i+1) #index(x,i+1)是从num1后的序列后找num2                
+                break
+    if j>0:
+        return [i,j]
+    else:
+        return []
 
-
+```
+soluton2:
+def twoSum(nums, target):
+    lens = len(nums)
+    j=-1
+    for i in range(1,lens):
+        temp = nums[:i]
+        if (target - nums[i]) in temp:
+            j = temp.index(target - nums[i])
+            break
+    if j>=0:
+        return [j,i]
+```
