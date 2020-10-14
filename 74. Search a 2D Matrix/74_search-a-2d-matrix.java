96a3fccc5c912aa0f962fe1b611c315b1c04a564
Wei ZHANG <yidazhang1@gmail.com>
2020-10-14 19:49:52 +0800

    Create 74_search-a-2d-matrix.java

---
+++ 74. Search a 2D Matrix/74_search-a-2d-matrix.java
@@ -0,0 +1,21 @@
+class Solution {
+  public boolean searchMatrix(int[][] matrix, int target) {
+    int m = matrix.length;
+    if (m == 0) return false;
+    int n = matrix[0].length;
+
+    // 二分查找
+    int left = 0, right = m * n - 1;
+    int pivotIdx, pivotElement;
+    while (left <= right) {
+      pivotIdx = (left + right) / 2;
+      pivotElement = matrix[pivotIdx / n][pivotIdx % n];
+      if (target == pivotElement) return true;
+      else {
+        if (target < pivotElement) right = pivotIdx - 1;
+        else left = pivotIdx + 1;
+      }
+    }
+    return false;
+  }
+}
