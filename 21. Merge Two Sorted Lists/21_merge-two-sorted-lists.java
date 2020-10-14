a3cb10cad4d45887e615b4544f94fd7aa82c1deb
Wei ZHANG <yidazhang1@gmail.com>
2020-10-14 20:13:41 +0800

    Update and rename 1_two-sum.java to 21_merge-two-sorted-lists.java

---
+++ 21. Merge Two Sorted Lists/21_merge-two-sorted-lists.java
@@ -0,0 +1,35 @@
+class Solution {
+    //solution1：recursion
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+  
+    //solution2：iteration;
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        prev.next = l1 == null ? l2 : l1;
+        return prehead.next;
+    }
+}
