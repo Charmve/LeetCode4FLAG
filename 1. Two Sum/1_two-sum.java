7e5ed37b719141a9db07f6526cc083d03526d079
Wei ZHANG <yidazhang1@gmail.com>
2020-10-14 20:28:53 +0800

    Update 1_two-sum.java

--- 1. Two Sum/1_two-sum.java
+++ 1. Two Sum/1_two-sum.java
@@ -1,21 +1,26 @@
 class Solution {
-  public boolean searchMatrix(int[][] matrix, int target) {
-    int m = matrix.length;
-    if (m == 0) return false;
-    int n = matrix[0].length;
-
-    // binary search
-    int left = 0, right = m * n - 1;
-    int pivotIdx, pivotElement;
-    while (left <= right) {
-      pivotIdx = (left + right) / 2;
-      pivotElement = matrix[pivotIdx / n][pivotIdx % n];
-      if (target == pivotElement) return true;
-      else {
-        if (target < pivotElement) right = pivotIdx - 1;
-        else left = pivotIdx + 1;
-      }
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
     }
-    return false;
-  }
+}
+
+class Solution2 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[0];
+    }
 }
