18a62663e515cba9b2ac5ed958af4c9d1b29f4e0
Wei ZHANG <yidazhang1@gmail.com>
2021-09-14 19:08:01 +0800

    Update 202_happy-number.cpp

--- 202. Happy Number/202_happy-number.cpp
+++ 202. Happy Number/202_happy-number.cpp
@@ -1,1 +1,26 @@
 
+int isInSeq(int n)
+{
+    int a = 0; 
+    int sum = 0;
+    while(n)
+    {
+        a = n%10;
+        sum += a*a;
+        n = n/10;
+    }
+    return sum;
+}
+
+int isHappy(int n){
+    int slow = n;
+    int fast = n;
+
+    do{
+        slow = isInSeq(slow);
+        fast = isInSeq(fast);
+        fast = isInSeq(fast);
+    }while(slow!=fast);
+
+    return slow == 1;
+}
